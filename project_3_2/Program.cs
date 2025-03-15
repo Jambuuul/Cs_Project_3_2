72dc996fe70371aa239083f151717e5de2118126
Jam <jamalabdulov@mail.ru>
2025-03-15 21:01:49 +0300

    minor changes

--- project_3_2/Program.cs
+++ project_3_2/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Hello, World!");
         }
