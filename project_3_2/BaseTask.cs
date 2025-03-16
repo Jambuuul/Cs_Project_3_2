5f55a74bcd5265bceab2eeff22a34020c11dd916
Jam <jamalabdulov@mail.ru>
2025-03-16 20:21:11 +0300

    making base menu

--- project_3_2/BaseTask.cs
+++ project_3_2/BaseTask.cs
@@ -10,9 +10,61 @@ namespace project_3_2
     {
         public static void Run(ref Cities cities)
         {
+            while (true)
+            {
+                Console.Clear();
+                BaseMenu();
+                string? input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Clear();
+                        cities.PrintInfo();
+                        Program.AskForInput(); break;
+                    case "2":
+                        AddCity(ref cities); ; break;
+                    case "3":
+                        AdditionalTask.Run(); break;
+                    case "4":
+                        break;
+                    case "5":
+                        System.Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Некорректная команда!");
+                        break;
+                }
+            }
+        }
+
+        public static void AddCity(ref Cities cities)
+        {
             Console.Clear();
-            
-            cities.PrintInfo();
+            Console.WriteLine("Введите в типичном формате строку с данными о городе:");
+            string? input = Console.ReadLine();
+            if (input == null || !City.TryParse(input, out City? city) || city is null)
+            {
+                Console.WriteLine("Ошибка при добавлении города!");
+                Program.AskForInput();
+                return;
+            }
+            cities.AddCity(city);
+        }
+
+        
+
+        public static void BaseMenu()
+        {
+            Console.WriteLine("Базовый функционал приложения:");
+            Console.WriteLine("" +
+                "1. Просмотр списка городов\n" +
+                "2. Добавление нового города\n" +
+                "3. Редактирование информации о городе\n" +
+                "4. Удаление города\n" +
+                "5. Выход в главное меню");
+
+            Console.Write("Выберите опцию: ");
         }
     }
 }
